09b6c20322dd7de9bab96f72cf22eab4fddddcce
satyam saxena <satyamsaxena@outlook.com>
2020-09-06 12:54:07 +0530

    initial commit

---
+++ src/components/Titles.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Titles = () => (
+    <div>
+        <h1 className="title-container__title">Weather Application in React</h1>
+        <p className="title-container__subtitle">Check Weather Upadate...</p>
+    </div>
+);
+export default Titles;
